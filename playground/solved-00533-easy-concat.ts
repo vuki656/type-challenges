3e3ded080f1213bb76edf715d82042c84418b5d4
domagoj.vukovic <off.vukovic@gmail.com>
2023-02-16 23:26:44 +0100

    solve 00533

--- playground/solved-00533-easy-concat.ts
+++ playground/solved-00533-easy-concat.ts
@@ -18,7 +18,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Concat<T, U> = any
+type Concat<T extends any[], U extends any[]> = [...T, ...U]
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
