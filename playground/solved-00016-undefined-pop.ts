a76b967cbd1c355bad4b79da1c7c56c6114e0531
domagoj.vukovic <off.vukovic@gmail.com>
2023-02-18 17:05:06 +0100

    solve 00016

--- playground/solved-00016-undefined-pop.ts
+++ playground/solved-00016-undefined-pop.ts
@@ -26,7 +26,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Pop<T extends any[]> = any
+type Pop<T extends any[]> = T extends [...infer Other, infer _Last] ? Other : T
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
